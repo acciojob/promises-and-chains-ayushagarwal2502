adabc549f151be120055f26a39c819946be5e0d3
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-03-01 18:04:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+function submitForm(){
+	let Name=document.getElementById("name").value;
+	let Age=document.getElementById("age").value;
+	console.log(Name)
+	     if (Name == "" ||Age=="" ) {
+    alert("Please enter valid details.")
+  }
+	const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (Age>18) {
+      resolve("Welcome, . You can vote.");
+    } else {
+      reject("Oh sorry . You aren't old enough.");
+    }
+  }, 4000);
+});
+
+promise.then(result => {
+   alert(result);
+}).catch(error => {
+    alert(err);
+});
+
+}
